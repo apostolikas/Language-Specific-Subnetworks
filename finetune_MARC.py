7868d18ce92c5851c37c163be02a72b6bc631805
Nick Apostolikas <n.apostolikas@hotmail.com>
2023-04-30 11:51:00 +0200

    code refactored

--- finetune_MARC.py
+++ finetune_MARC.py
@@ -1,170 +1,121 @@
+from transformers import AutoTokenizer
 from datasets import load_dataset, concatenate_datasets
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer, PreTrainedTokenizer
-#import argparse
 import torch
-from dataclasses import dataclass
-from torch.utils.data.dataset import Dataset
-from typing import Dict
-from typing import List
-import numpy as np
+from dataclasses import dataclass, field
+from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer, DataCollator
 import evaluate
-
-dataset_en = load_dataset("amazon_reviews_multi", "en")
-dataset_de = load_dataset("amazon_reviews_multi", "de")
-dataset_fr = load_dataset("amazon_reviews_multi", "fr")
-dataset_es = load_dataset("amazon_reviews_multi", "es")
-dataset_zh = load_dataset("amazon_reviews_multi", "zh")
-
-def process_columns(dataset):
-    wanted_columns = ['review_body', 'stars']
-    for column_name in dataset['train'].features.keys():
-        if column_name not in wanted_columns:
-            dataset = dataset.remove_columns(column_name)
-    dataset = dataset.rename_column("review_body", "text")
-    dataset = dataset.rename_column("stars", "label")
-    return dataset
-
-dataset_en = process_columns(dataset_en)
-dataset_de = process_columns(dataset_de)
-dataset_fr = process_columns(dataset_fr)
-dataset_es = process_columns(dataset_es)
-dataset_zh = process_columns(dataset_zh)
-
-train_multilingual_dataset = concatenate_datasets([dataset_en['train'], dataset_de['train'], dataset_fr['train'], dataset_es['train'], dataset_zh['train']])
-val_multilingual_dataset = concatenate_datasets([dataset_en['validation'], dataset_de['validation'], dataset_fr['validation'], dataset_es['validation'], dataset_zh['validation']])
-test_multilingual_dataset = concatenate_datasets([dataset_en['test'], dataset_de['test'], dataset_fr['test'], dataset_es['test'], dataset_zh['test']])
-
-
-tokenizer = AutoTokenizer.from_pretrained("xlm-roberta-base")
-# def tokenize_fn(dataset):
-#     return tokenizer(dataset['text'], padding='max_length', truncation=True)
-# train_set = train_multilingual_dataset.map(tokenize_fn, batched=True)
-# train_set = train_set.map(lambda example: {"label": example["label"] - 1})
-# val_set = val_multilingual_dataset.map(tokenize_fn, batched=True)
-# val_set = val_set.map(lambda example: {"label": example["label"] - 1})
-# test_set = test_multilingual_dataset.map(tokenize_fn, batched=True)
-# test_set = test_set.map(lambda example: {"label": example["label"] - 1})
-
-train_set = train_multilingual_dataset.map(lambda example: {"label": example["label"] - 1})
-val_set = val_multilingual_dataset.map(lambda example: {"label": example["label"] - 1})
-test_set = test_multilingual_dataset.map(lambda example: {"label": example["label"] - 1})
-
+import numpy as np
 
 @dataclass
-class Example:
-    text: str
-    label: int
+class SmartCollator():
+  pad_token_id: int
 
-#train_set = [Example(text=item["premise"], text_b=item["hypothesis"], label=item["label"]) for item in train_set["train"]]
-
-train_dataset = [Example(text=item["text"], label=item["label"]) for item in train_set]
-val_dataset = [Example(text=item["text"], label=item["label"]) for item in val_set]
-
-@dataclass
-class Features:
-    input_ids: List[int]
-    attention_mask: List[int]
-    label: int
-
-class TextDataset(Dataset):
-    def __init__(self, tokenizer: PreTrainedTokenizer, 
-                 pad_to_max_length: bool, 
-                 max_len: int,
-                 examples: List[Example]) -> None:
-        self.tokenizer = tokenizer
-        self.max_len = max_len
-        self.examples: List[Example] = examples
-        self.current = 0
-        self.pad_to_max_length = pad_to_max_length
-
-    def encode(self, ex: Example) -> Features:
-        encode_dict = self.tokenizer.encode_plus(text=ex.text,
-                                                 add_special_tokens=True,
-                                                 max_length=self.max_len,
-                                                 pad_to_max_length=self.pad_to_max_length,
-                                                 return_token_type_ids=False,
-                                                 return_attention_mask=True,
-                                                 return_overflowing_tokens=False,
-                                                 return_special_tokens_mask=False,
-                                                 )
-        return Features(input_ids=encode_dict["input_ids"],
-                        attention_mask=encode_dict["attention_mask"],
-                        label=ex.label)
-
-    def __getitem__(self, idx) -> Features:
-        return self.encode(ex=self.examples[idx])
-
-    def __len__(self):
-        return len(self.examples)
+  def __call__(self, batch):
+    batch_inputs = list()
+    batch_attention_masks = list()
+    labels = list()
+    max_size = max([len(ex['input_ids']) for ex in batch])
+    for item in batch:
+        batch_inputs += [pad_seq(item['input_ids'], max_size, self.pad_token_id)]
+        batch_attention_masks += [pad_seq(item['attention_mask'], max_size, 0)]
+        labels.append(item['labels'])
+    return {"input_ids": torch.tensor(batch_inputs, dtype=torch.long),
+            "attention_mask": torch.tensor(batch_attention_masks, dtype=torch.long),
+            "labels": torch.tensor(labels, dtype=torch.long)
+            } 
 
 
-def pad_seq(seq: List[int], max_batch_len: int, pad_value: int) -> List[int]:
-    return seq + (max_batch_len - len(seq)) * [pad_value]
-
-@dataclass
-class CustomTransformation():
-    pad_token_id: int
-
-    def __call__(self, batch: List[Features]) -> Dict[str, torch.Tensor]:
-        batch_inputs = list()
-        batch_attention_masks = list()
-        labels = list()
-        max_size = max([len(ex.input_ids) for ex in batch])
-        for item in batch:
-            batch_inputs += [pad_seq(item.input_ids, max_size, self.pad_token_id)]
-            batch_attention_masks += [pad_seq(item.attention_mask, max_size, 0)]
-            labels.append(item.label)
-
-        return {"input_ids": torch.tensor(batch_inputs, dtype=torch.long),
-                "attention_mask": torch.tensor(batch_attention_masks, dtype=torch.long),
-                "labels": torch.tensor(labels, dtype=torch.long)
-                }
-
-model = AutoModelForSequenceClassification.from_pretrained("xlm-roberta-base", num_labels=5)
-metric = evaluate.load("accuracy")
-
-def compute_metrics(eval_pred):
-    logits, labels = eval_pred
-    predictions = np.argmax(logits, axis=-1)
-    return metric.compute(predictions=predictions, references=labels)
-
-training_args = TrainingArguments(output_dir="test_trainer",
-                                  evaluation_strategy='steps',
-                                  eval_steps=2000,
-                                  learning_rate=5e-5,
-                                  num_train_epochs=1,
-                                  logging_steps=1000,
-                                  seed=42
-                                  )
-
-train_data = TextDataset(tokenizer=tokenizer,
-                        max_len=512,
-                        examples=train_dataset,
-                        pad_to_max_length=False)  
-                        
-valid_data = TextDataset(tokenizer=tokenizer,
-                        max_len=512,
-                        examples=val_dataset,
-                        pad_to_max_length=False)  
+def pad_seq(seq,max_batch_len, pad_value):
+  return seq + (max_batch_len - len(seq))*[pad_value]  
 
 
-# trainer = Trainer(
-#     model=model,
-#     args=training_args,
-#     train_dataset=train_set,
-#     eval_dataset=val_set,
-#     compute_metrics=compute_metrics,
-# )
 
-train_collator = CustomTransformation(pad_token_id=tokenizer.pad_token_id)
+def fetch_datasets():
+    dataset_en = load_dataset("amazon_reviews_multi", "en")
+    dataset_de = load_dataset("amazon_reviews_multi", "de")
+    dataset_fr = load_dataset("amazon_reviews_multi", "fr")
+    dataset_es = load_dataset("amazon_reviews_multi", "es")
+    dataset_zh = load_dataset("amazon_reviews_multi", "zh")
+    train_dataset = concatenate_datasets([dataset_en['train'], dataset_de['train'], dataset_fr['train'], dataset_es['train'], dataset_zh['train']])
+    val_dataset = concatenate_datasets([dataset_en['validation'], dataset_de['validation'], dataset_fr['validation'], dataset_es['validation'], dataset_zh['validation']])
+    test_dataset = concatenate_datasets([dataset_en['test'], dataset_de['test'], dataset_fr['test'], dataset_es['test'], dataset_zh['test']])
+    return train_dataset, val_dataset, test_dataset
 
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=train_data,
-    data_collator=train_collator,
-    eval_dataset=valid_data,
-    compute_metrics=compute_metrics,
-)
 
-trainer.train()
+
+tokenizer = AutoTokenizer.from_pretrained('xlm-roberta-base', use_fast=True)
+model = AutoModelForSequenceClassification.from_pretrained('xlm-roberta-base', num_labels=5)
+
+
+if __name__ == "__main__":    
+    
+    train_dataset, val_dataset, test_dataset = fetch_datasets()
+    print("--- Data loaded successfully ---")
+
+    def tokenize_data(ex_dataset):
+        # Concatenate review body with review_title and product_category for performance boost 
+        text = ex_dataset['review_body'] + " " + ex_dataset['review_title'] + " " + ex_dataset['product_category']
+        # Tokenize & Encode
+        encodings = tokenizer.encode_plus(text, 
+                                        pad_to_max_length=False,
+                                        max_length=512,
+                                        add_special_tokens=True,
+                                        return_token_type_ids=False,
+                                        return_attention_mask=True,
+                                        return_overflowing_tokens=False,
+                                        return_special_tokens_mask=False,
+                                        )
+        # Labels in range 0-4
+        targets = torch.tensor(ex_dataset['stars']-1,dtype=torch.long)
+        encodings.update({'labels': targets})
+        return encodings
+
+
+    def compute_metrics(eval_pred):
+        predictions, labels = eval_pred
+        predictions = np.argmax(predictions, axis=1)
+        return metric.compute(predictions=predictions, references=labels)
+
+
+    encoded_train_dataset = list(map(tokenize_data,train_dataset)) #encoding the dataset by mapping the tokenize function to each sample in the dataset
+    encoded_val_dataset = list(map(tokenize_data,val_dataset))
+    encoded_test_dataset = list(map(tokenize_data,test_dataset))
+    print("--- Data processed successfully ---")
+
+    batch_size = 32
+    metric_name = "accuracy"
+
+    args = TrainingArguments(  #Defining Training arguments
+        output_dir = "Finetune_MARC_results", #Storing model checkpoints 
+        seed = 42, 
+        evaluation_strategy = "steps",
+        learning_rate=5e-5,
+        per_device_train_batch_size=batch_size,
+        per_device_eval_batch_size=batch_size,
+        num_train_epochs=1,
+        weight_decay=0.01,
+        load_best_model_at_end=True, #During evaluation the model with highest accuracy will be loaded.
+        metric_for_best_model=metric_name,
+        eval_steps = 5000,
+        save_steps = 5000,
+        fp16 = True
+    )
+    metric = evaluate.load('accuracy')
+
+    trainer = Trainer(
+        model,
+        args,
+        train_dataset= encoded_train_dataset,
+        eval_dataset=encoded_val_dataset,
+        data_collator=SmartCollator(pad_token_id=tokenizer.pad_token_id),
+        tokenizer=tokenizer,
+        compute_metrics=compute_metrics
+        )       
+
+    "--- Trainer finetune ---"
+    trainer.train() #training
+    "--- Trainer check model ---"
+    trainer.evaluate() #Checking to see if model with highest accuracy is returned
+    "--- Trainer evaluate on testset ---"
+    trainer.evaluate(encoded_test_dataset) #evaluating performance on test dataset. 
+
