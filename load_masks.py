7aa3662299008e667a0014ef18698b065de70a28
vasilisvyth <72560545+vasilisvyth@users.noreply.github.com>
2023-05-18 19:06:39 +0200

    remove some code

--- load_masks.py
+++ load_masks.py
@@ -8,7 +8,7 @@ import itertools
 import statistics
 import json
 import matplotlib.pyplot as plt
-
+from make_plots import plot_lower_triangular_matrix
 LANGUAGES = ['en','de','fr','es','zh']
 TASKS = ['marc','paws-x','xnli']
 NUM_SEEDS = 5
@@ -152,41 +152,6 @@ def jaccard_similarity_per_seed_pair(masks_dict):
 
     return stats_jaccard_seed_dict
 
-def plot_matrix(matrix, x_labels, y_labels, save_path, x_title, y_title, colour_map=plt.cm.OrRd):
-    initial_matrix = matrix
-
-    mask =  np.tri(matrix.shape[0], k=-1).T
-    matrix = np.ma.array(matrix, mask=mask)
-
-    fig, ax = plt.subplots(figsize=(9,8))
-    ax.set_xlabel(x_title, labelpad=20)
-    ax.set_ylabel(y_title)
-    ax.xaxis.set_ticks_position('bottom')
-    ax.xaxis.set_label_position('bottom')
-    ax.set_xticks(range(len(x_labels)))
-    ax.set_yticks(range(len(y_labels)))
-    ax.set_xticklabels(x_labels)
-    ax.set_yticklabels(y_labels)
-    # Hide the right and top spines
-    ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible(False)
-    
-    mask =  np.tri(matrix.shape[0], k=0).T # Leave Diagonal Uncoloured
-    mean_matrix_wo_diagonal = np.ma.array(matrix, mask=mask) 
-    
-    ax.imshow(mean_matrix_wo_diagonal, cmap=colour_map)
-    plt.gca().xaxis.tick_bottom()
-    
-    
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            if i <= j:
-                mean = initial_matrix[i, j]
-                text = f"{mean:.2f}"
-                # color = 'black' if mean < mean_mean or i == j else 'white'
-                ax.text(i, j, text, va='center', ha='center', color='black')
-    if save_path:
-        fig.savefig(save_path)
 
 def intersection(lst1, lst2):
     lst3 = [value for value in lst1 if value in lst2]
@@ -214,7 +179,7 @@ def surviving_per_task(masks_dict):
             overlap_lang[task_id, task_id] = len(heads_ids[task_id])
         for task_id1, task_id2 in itertools.combinations([k for k in range(len(TASKS))], r=2):
             overlap_lang[task_id1, task_id2] =  len(intersection(heads_ids[task_id1], heads_ids[task_id2]))
-        plot_matrix(overlap_lang, TASKS, TASKS, f'super_survivors_{lang}', 'TASK', 'TASK')
+        plot_lower_triangular_matrix(overlap_lang, TASKS, TASKS, f'super_survivors_{lang}', 'TASK', 'TASK')
 
 
 masks_dict = load_masks()
