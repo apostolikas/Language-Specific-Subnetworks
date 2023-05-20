a6de50cf66bdd22c2181c4748410e0ca3e0ed283
Nick Apostolikas <n.apostolikas@hotmail.com>
2023-05-20 14:42:59 +0200

    better plots

--- stats_scripts.py
+++ stats_scripts.py
@@ -1,6 +1,9 @@
 import torch
 import os
+import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 LANGUAGES = ['en','de','fr','es','zh']
 TASKS = ['marc','paws-x','xnli']
@@ -69,15 +72,32 @@ def visualize_dictionaries(input_dict:dict, mode:str):
                 break
             ax = axs[row, col]  
             if mode == 'mask':
+                
                 ax.imshow(tensor, cmap='Blues')  # heatmap for masks
+                ax.set_ylabel('Layer')
+                ax.set_xlabel('Head')
+
+                ax.set_xticks(np.arange(0, 12))
+                ax.set_yticks(np.arange(0, 12))
+                ax.set_xticklabels(np.arange(1, 13))
+                ax.set_yticklabels(np.arange(1, 13))
             elif mode == 'importance':
-                ax.bar(tensor[1]+1,tensor[0]) # barplot with layers with most used heads
+                cmap = plt.cm.get_cmap('plasma')
+                normalized_y = 1 - (tensor[0]) / (12)
+                ax.bar(tensor[1],tensor[0],color=cmap(normalized_y)) # barplot with layers with most used heads
+                ax.set_ylabel('Heads in use')
+                ax.set_xlabel('Layer')
+                ax.set_xticks(np.arange(0, 12))
+                ax.set_yticks(np.arange(0, 13))
+                ax.set_xticklabels(np.arange(1, 13))
+                #ax.set_yticklabels(np.arange(1, 13))
             ax.set_title(f'Task: {key}, Language: {inner_key}')  
             col += 1
         row += 1
         col = 0
     plt.tight_layout() 
-    plt.show()  
+    plt.show()
+    
 
 if __name__ == '__main__':
     masks_dict = load_masks()
