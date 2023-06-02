5aada0658af4616ef1f68a3ddc5d133cf75af360
Gergely Papp <gergopool@gmail.com>
2023-06-02 09:50:28 +0000

    download script adjusted to new gdrive link

--- download.sh
+++ download.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 root=results
-drive_link=https://drive.google.com/file/d/1AUcMuQZkXixQoqN92ZopumoATcXF39pc/view?usp=drive_link
+drive_link=https://drive.google.com/file/d/14xYRVCJbhxhkGR85JzizXn0Me-mMgEKa/view?usp=sharing
 
 if [ ! -f $root/models/xnli/best/config.json ]
 then
     echo "Downloading results file.."
     python src/drive_download.py $drive_link
-    tar -xzvf results.tar.gz
-    rm results.tar.gz
+    unzip results.zip
+    rm results.zip
     echo "Done."
 else
     echo "Results folder found, with content. Skipping download."
